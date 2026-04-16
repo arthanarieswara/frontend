11d848e6279ab41d8c8465877ef8af935d478e58
Arthanarieswara <arthanarieswara44@gmail.com>
2026-04-16 12:35:55 +0530

    first commit

--- .history/src/api/api_20260416123422.js
+++ .history/src/api/api_20260416123422.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-px4u.onrender.com/api",
+  baseURL: "https://backend-px4u.onrender.com/",
 });
 
 /* ✅ Attach token automatically */
